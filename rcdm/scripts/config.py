3b3ba3b526ce6a702e99f850a1d2e0adbeedc11d
Vahid Reza Khazaie <vrkh1996@gmail.com>
2023-11-28 13:31:30 -0500

    add comment to config file

--- rcdm/scripts/config.py
+++ rcdm/scripts/config.py
@@ -2,24 +2,24 @@ import ml_collections
 
 def get_config():
     config = ml_collections.ConfigDict()
-    config.image_size = 128
-    config.class_cond = False
-    config.type_model = "simclr"
-    config.use_head = False
-    config.model_path = ""  # Replace with the path to your model if you have one
-    config.num_images = 2
-    config.use_ddim = False
-    config.no_shared = True
-    config.clip_denoised = True
-    config.attention_resolutions = "32,16,8"
-    config.diffusion_steps = 1000
-    config.learn_sigma = True
-    config.noise_schedule = "linear"
-    config.num_channels = 256
-    config.num_heads = 4
-    config.num_res_blocks = 2
-    config.resblock_updown = True
-    config.use_fp16 = False
-    config.use_scale_shift_norm = True
-    
+    config.image_size = 128  # The size of the images to generate.
+    config.class_cond = False  # If true, use class conditional generation.
+    config.type_model = "simclr"  # Type of model to use (e.g., simclr, dino).
+    config.use_head = False  # If true, use the projector/head for SSL representation.
+    config.model_path = ""  # Replace with the path to your model if you have one.
+    config.num_images = 2  # Number of images to generate.
+    config.use_ddim = False  # If true, use DDIM sampler.
+    config.no_shared = True  # If false, enables squeeze and excitation.
+    config.clip_denoised = True  # If true, clip denoised images.
+    config.attention_resolutions = "32,16,8"  # Resolutions to use for attention layers.
+    config.diffusion_steps = 1000  # Number of diffusion steps.
+    config.learn_sigma = True  # If true, learn the noise level.
+    config.noise_schedule = "linear"  # Type of noise schedule (e.g., linear).
+    config.num_channels = 256  # Number of channels in the model.
+    config.num_heads = 4  # Number of attention heads.
+    config.num_res_blocks = 2  # Number of residual blocks.
+    config.resblock_updown = True  # If true, use up/down sampling in resblocks.
+    config.use_fp16 = False  # If true, use 16-bit floating point precision.
+    config.use_scale_shift_norm = True  # If true, use scale-shift normalization.
+
     return config

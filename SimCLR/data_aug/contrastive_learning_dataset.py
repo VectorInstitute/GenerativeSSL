bb0a3133a6944468fc3bbc1f96b55c0c8e6f1fd6
sanaAyrml <ayromlous@gmail.com>
2023-12-02 18:06:45 -0800

    edit

--- SimCLR/data_aug/contrastive_learning_dataset.py
+++ SimCLR/data_aug/contrastive_learning_dataset.py
@@ -1,7 +1,7 @@
 from torchvision.transforms import transforms
 from SimCLR.data_aug.gaussian_blur import GaussianBlur
 from SimCLR.data_aug.rcdm_aug import RCDMInference
-# from .rcdm_config import get_config
+from SimCLR.data_aug.rcdm_config import get_config
 from torchvision import transforms, datasets
 from SimCLR.data_aug.view_generator import ContrastiveLearningViewGenerator
 from SimCLR.exceptions.exceptions import InvalidDatasetSelection
@@ -21,12 +21,12 @@ class ContrastiveLearningDataset:
                           transforms.RandomGrayscale(p=0.2),
                           GaussianBlur(kernel_size=int(0.1 * size)),
                           transforms.ToTensor()]
-        # if rcdm_agumentation:
-        #     rcdm_config = get_config()
-        #     transform_list.append(RCDMInference(rcdm_config))
-        #     transform_list.append(transforms.Resize(size=(size, size)))
+        if rcdm_agumentation:
+            rcdm_config = get_config()
+            transform_list.append(RCDMInference(rcdm_config))
+            transform_list.append(transforms.Resize(size=(size, size)))
 
-        # data_transforms = transforms.Compose(transform_list)
+        data_transforms = transforms.Compose(transform_list)
         return data_transforms
 
     def get_dataset(self, name, n_views):

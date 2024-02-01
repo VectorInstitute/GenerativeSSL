9ae6ee87e5c309c6fa53b471b908ddc30866f74c
sanaAyrml <ayromlous@gmail.com>
2024-01-31 16:56:56 -0800

    Add eval files

---
+++ SimCLR/evaluate_simCLR.py
@@ -0,0 +1,242 @@
+import argparse
+import random
+from functools import partial
+
+import torch
+from torch.nn.parallel import DistributedDataParallel as DDP  # noqa: N817
+from torch.utils.data.distributed import DistributedSampler
+from torchvision import models
+
+from SimCLR import distributed as dist_utils
+from SimCLR.data_aug.supervised_dataset import SupervisedDataset
+from SimCLR.models.resnet_pretrained import PretrainedResNet
+from SimCLR.simclr import SimCLR
+
+
+model_names = sorted(
+    name
+    for name in models.__dict__
+    if name.islower() and not name.startswith("__") and callable(models.__dict__[name])
+)
+
+parser = argparse.ArgumentParser(description="PyTorch SimCLR")
+parser.add_argument(
+    "-data",
+    metavar="DIR",
+    default="/scratch/ssd004/datasets/imagenet256",
+    help="path to dataset, for imagenet: /scratch/ssd004/datasets/imagenet256 ",
+)
+parser.add_argument(
+    "-dataset-name",
+    default="imagenet",
+    help="dataset-name",
+    choices=["stl10", "cifar10", "imagenet"],
+)
+parser.add_argument(
+    "-a",
+    "--arch",
+    metavar="ARCH",
+    default="resnet18",
+    choices=model_names,
+    help="model architecture: " + " | ".join(model_names) + " (default: resnet18)",
+)
+parser.add_argument(
+    "-j",
+    "--num_workers",
+    default=4,
+    type=int,
+    metavar="N",
+    help="number of data loading workers",
+)
+parser.add_argument(
+    "--epochs", default=100, type=int, metavar="N", help="number of total epochs to run"
+)
+parser.add_argument(
+    "-b",
+    "--batch-size",
+    default=64,
+    type=int,
+    metavar="N",
+    help="mini-batch size (default: 256), this is the total "
+    "batch size of all GPUs on the current node when "
+    "using Data Parallel or Distributed Data Parallel",
+)
+
+parser.add_argument(
+    "--seed", default=42, type=int, help="seed for initializing training. "
+)
+
+parser.add_argument(
+    "--log-every-n-steps", default=100, type=int, help="Log every n steps"
+)
+parser.add_argument(
+    "--distributed_mode", action="store_true", help="Enable distributed training"
+)
+parser.add_argument("--distributed_launcher", default="slurm")
+parser.add_argument("--distributed_backend", default="nccl")
+parser.add_argument("--model_dir", default="model_checkpoints")
+parser.add_argument("--experiment_name", default="simclr")
+parser.add_argument("--linear_evaluation", 
+                    action="store_true",
+                    help="Whether or not to evaluate the linear evaluation of the model.")
+
+def worker_init_fn(worker_id: int, num_workers: int, rank: int, seed: int) -> None:
+    """Initialize worker processes with a random seed.
+
+    Parameters
+    ----------
+    worker_id : int
+        ID of the worker process.
+    num_workers : int
+        Total number of workers that will be initialized.
+    rank : int
+        The rank of the current process.
+    seed : int
+        A random seed used determine the worker seed.
+    """
+    worker_seed = num_workers * rank + worker_id + seed
+    torch.manual_seed(worker_seed)
+    random.seed(worker_seed)
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def main():
+    args = parser.parse_args()
+    print(args)
+
+    torch.multiprocessing.set_start_method("spawn")
+
+    assert (
+        args.n_views == 2
+    ), "Only two view training is supported. Please use --n-views 2." 
+
+    if args.distributed_mode:
+        dist_utils.init_distributed_mode(
+            launcher=args.distributed_launcher,
+            backend=args.distributed_backend,
+        )
+        device_id = torch.cuda.current_device()
+    else:
+        device_id = None
+
+    dataset = SupervisedDataset(args.data)
+    train_dataset = dataset.get_dataset(
+        name = args.dataset_name,
+        train=True,
+    )
+    test_dataset = dataset.get_dataset(
+        name = args.dataset_name,
+        train=False,
+    )
+    train_sampler = None
+    test_sampler = None
+
+    if dist_utils.is_dist_avail_and_initialized() and args.distributed_mode:
+            train_sampler = DistributedSampler(
+                train_dataset,
+                seed=args.seed,
+                drop_last=True,
+            )
+            test_sampler = DistributedSampler(
+                test_dataset,
+                seed=args.seed,
+                drop_last=False,
+            )
+
+    init_fn = partial(
+        worker_init_fn,
+        num_workers=args.num_workers,
+        rank=dist_utils.get_rank(),
+        seed=args.seed,
+    )
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=(train_sampler is None),
+        sampler=train_sampler,
+        num_workers=args.num_workers,
+        worker_init_fn=init_fn,
+        pin_memory=False,
+        drop_last=True,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=args.batch_size,
+        shuffle=(test_sampler is None),
+        sampler=test_sampler,
+        num_workers=args.num_workers,
+        worker_init_fn=init_fn,
+        pin_memory=False,
+        drop_last=False,
+    )
+
+    model = PretrainedResNet(base_model=args.arch, pretrained_dir = args.model_dir, linear_eval=args.linear_evaluation)
+
+    if args.distributed_mode and dist_utils.is_dist_avail_and_initialized():
+        # set the single device scope, otherwise DistributedDataParallel will
+        # use all available devices
+        torch.cuda.set_device(device_id)
+        model = model.cuda(device_id)
+        model = DDP(model, device_ids=[device_id])
+    else:
+        model = model.cuda()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=3e-4, weight_decay=0.0008)
+    criterion = torch.nn.CrossEntropyLoss().cuda(device_id)
+
+    for epoch in range(args.epochs):
+        if dist_utils.is_dist_avail_and_initialized():
+            train_loader.sampler.set_epoch(epoch)
+        top1_train_accuracy = 0
+        for counter, (x_batch, y_batch) in enumerate(train_loader):
+            x_batch = x_batch.cuda(device_id)
+            y_batch = y_batch.cuda(device_id)
+
+            logits = model(x_batch)
+            loss = criterion(logits, y_batch)
+            top1 = accuracy(logits, y_batch, topk=(1,))
+            top1_train_accuracy += top1[0]
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        top1_train_accuracy /= counter + 1
+        top1_accuracy = 0
+        top5_accuracy = 0
+        for counter, (x_batch, y_batch) in enumerate(test_loader):
+            x_batch = x_batch.cuda(device_id)
+            y_batch = y_batch.cuda(device_id)
+
+            logits = model(x_batch)
+
+            top1, top5 = accuracy(logits, y_batch, topk=(1, 5))
+            top1_accuracy += top1[0]
+            top5_accuracy += top5[0]
+
+        top1_accuracy /= counter + 1
+        top5_accuracy /= counter + 1
+        print(
+            f"Epoch {epoch}\tTop1 Train accuracy {top1_train_accuracy.item()}\tTop1 Test accuracy: {top1_accuracy.item()}\tTop5 test acc: {top5_accuracy.item()}",
+        )
+
+   
+
+
+if __name__ == "__main__":
+    main()
---
+++ SimCLR/models/resnet_pretrained.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+from torchvision import models
+
+from ..exceptions.exceptions import InvalidBackboneError
+
+
+class PretrainedResNet(nn.Module):
+    def __init__(self, base_model, pretrained_dir, linear_eval=True):
+        super(PretrainedResNet, self).__init__()
+
+        self.pretrained_dir = pretrained_dir
+
+        self.resnet_dict = {
+            "resnet18": models.resnet18(pretrained=False, num_classes=10),
+            "resnet50": models.resnet50(pretrained=False, num_classes=10),
+        }
+
+        self.backbone = self._get_basemodel(base_model)
+
+        # load pretrained weights
+        log = self._load_pretrained()
+        assert log.missing_keys == ["fc.weight", "fc.bias"]
+
+        if linear_eval:
+            # freeze all layers but the last fc
+            self._freeze_backbone()
+            parameters = list(filter(lambda p: p.requires_grad, self.backbone.parameters()))
+            assert len(parameters) == 2  # fc.weight, fc.bias
+
+    def _load_pretrained(self):
+        checkpoint = torch.load(self.pretrained_dir, map_location='cpu')
+        state_dict = checkpoint["state_dict"]
+        for k in list(state_dict.keys()):
+            if k.startswith("backbone."):
+                if k.startswith("backbone") and not k.startswith("backbone.fc"):
+                    # remove prefix
+                    state_dict[k[len("backbone.") :]] = state_dict[k]
+            del state_dict[k]
+        log = self.backbone.load_state_dict(state_dict, strict=False)
+        return log
+        
+
+    def _freeze_backbone(self):
+        # freeze all layers but the last fc
+        for name, param in self.backbone.named_parameters():
+            if name not in ["fc.weight", "fc.bias"]:
+                param.requires_grad = False
+        return
+
+
+    def _get_basemodel(self, model_name):
+        try:
+            model = self.resnet_dict[model_name]
+        except KeyError:
+            raise InvalidBackboneError(
+                "Invalid backbone architecture. Check the config file and pass one of: resnet18 or resnet50",
+            )
+        else:
+            return model
+
+    def forward(self, x):
+        return self.backbone(x)

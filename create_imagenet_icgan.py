8ff76a8202a996dd6acd88211bd444d1de01d9a9
Vahid Reza Khazaie <vrkh1996@gmail.com>
2024-02-23 17:10:07 -0500

    revise the script

--- create_imagenet_icgan.py
+++ create_imagenet_icgan.py
@@ -7,8 +7,43 @@ from icgan.icgan_inference import ICGANInference
 from pytorch_pretrained_biggan import convert_to_images
 from icgan.data_utils import utils as data_utils
 import torch
-
 import numpy as np
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--outdir",
+    type=str,
+    nargs="?",
+    help="dir to write results to",
+    default="/projects/imagenet_synthetic/synthetic_icgan"
+)
+parser.add_argument(
+    "--img_save_size",
+    type=int,
+    default=224,
+    help="image saving size"
+)
+parser.add_argument(
+    "--start",
+    type=int,
+    default=0,
+    help="start index",
+)
+parser.add_argument(
+    "--end",
+    type=int,
+    default=-1,
+    help="end index",
+)
+parser.add_argument(
+    "--ith_sample",
+    type=int,
+    default=0,
+    help="end index",
+)
+
+args = parser.parse_args()
 
 def save(out, torch_format=True):
     if torch_format:
@@ -18,7 +53,7 @@ def save(out, torch_format=True):
     return img
 
 def save_images(path, images, out_dir):
-    for j, img in enumerate(images):
+    for img in images:
         out_folder = path.split("/")[-1].split(".")[0].split("_")[0] # get the class name
         file_name = path.split("/")[-1].split(".")[0] # get the (class name_image number)
         save_folder = os.path.join(out_dir, out_folder) # create a folder for each class
@@ -26,56 +61,20 @@ def save_images(path, images, out_dir):
         if not os.path.exists(save_folder):
             os.makedirs(save_folder, exist_ok=True)
 
-        for j, img in enumerate(images):
-            save_file = os.path.join(save_folder, f"{file_name}_{j+1}.png")
-            img = save(img[np.newaxis, ...], torch_format=False)
-            img.save(save_file)
+        save_file = os.path.join(save_folder, f"{file_name}_{args.ith_sample}.JPEG")
+        pil_img = save(img[np.newaxis, ...], torch_format=False)
+        pil_img.thumbnail((args.img_save_size, args.img_save_size))
+        pil_img.save(save_file, format="JPEG")
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--outdir",
-        type=str,
-        nargs="?",
-        help="dir to write results to",
-        default="/projects/imagenet_synthetic/synthetic_icgan"
-    )
-    # parser.add_argument(
-    #     "--img_save_size",
-    #     type=int,
-    #     default=224,
-    #     help="image saving size"
-    # )
-    # parser.add_argument(
-    #     "--batch_size",
-    #     type=int,
-    #     default=1,
-    #     help="batch size",
-    # )
-    parser.add_argument(
-        "--start",
-        type=int,
-        default=0,
-        help="start index",
-    )
-    parser.add_argument(
-        "--end",
-        type=int,
-        default=-1,
-        help="end index",
-    )
-    args = parser.parse_args()
-
     if args.outdir is not None:
         os.makedirs(args.outdir, exist_ok=True)
-
     # Initialize the ICGANInference class
     config = get_config()
+    config.seed = args.ith_sample
     icgan_inference = ICGANInference(config)
-
     transform = transforms.Compose([
         data_utils.CenterCropLongEdge(),
-        # transforms.Resize((args.img_save_size, args.img_save_size)),
         transforms.ToTensor(),
         transforms.Normalize(config.norm_mean, config.norm_std)])
 
@@ -88,21 +87,13 @@ def main():
     assert args.start < n
     assert args.end <= n
 
-    for i in range(args.start, args.end):
-        print(f"Batch {i}")
+    for i in tqdm(range(args.start, args.end)):
         batch = imagenet_dataset[i]
         images = batch[0]
-        start = time.time()
         generated_images = icgan_inference.run_inference(input_image_tensor=images.unsqueeze(0))
-        end = time.time()
-        print(f"Time taken to generate images: {end-start} seconds")
-
         ## save images
         path = imagenet_dataset.samples[i][0]
-        start = time.time()
         save_images(path, generated_images, args.outdir)
-        end = time.time()
-        print(f"Time taken to save images: {end-start} seconds")
 
 if __name__ == "__main__":
     main()

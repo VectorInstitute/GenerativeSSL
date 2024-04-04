004f3db0edc5cb474f64c286314bc1a3df5a5644
sanaAyrml <ayromlous@gmail.com>
2024-04-04 12:08:49 -0400

    server

--- solo-learn/solo/data/dali_dataloader.py
+++ solo-learn/solo/data/dali_dataloader.py
@@ -39,6 +39,10 @@ from random import shuffle
 import random
 import numpy as np
 
+import cupy as cp
+import imageio
+
+
 
 class RandomSyntheticDataChoice:
     def __init__(self, prob: float = 0.2, len_choice = 1,device: str = "gpu"):
@@ -546,7 +550,6 @@ class ExternalInputIterator(object):
             files, _, labels, _ = train_test_split(
                 files, labels, train_size=data_fraction, stratify=labels, random_state=42
             )
-        
 
         if encode_indexes_into_labels:
             encoded_labels = []
@@ -565,6 +568,8 @@ class ExternalInputIterator(object):
         self.files = files
         self.labels = labels
         
+        self.synth_files = [[get_synthetic_image_path(jpeg_filename.absolute().as_posix(), self.synthetic_data_path, i ,split="train") for jpeg_filename in files ] for i in range(synthetic_index_min,synthetic_index_max+1)]
+        
         # whole data set size
         self.data_set_len = len(self.files)
 
@@ -577,11 +582,13 @@ class ExternalInputIterator(object):
 
     def __iter__(self):
         self.i = 0
+        self.indexes = np.arange(len(self.files))
         if self.random_shuffle:
-            indexes = np.arange(len(self.files))
-            np.random.shuffle(indexes)
-            self.files[:] = [self.files[i] for i in indexes]
-            self.labels[:] = [self.labels[i] for i in indexes]
+            np.random.shuffle(self.indexes)
+#             self.files[:] = [self.files[i] for i in indexes]
+#             self.labels[:] = [self.labels[i] for i in indexes]
+#             for j in range(len(self.synth_files)):
+#                 self.synth_files[j][:] = [self.synth_files[j][i] for i in indexes]
         return self
 
     def __next__(self):
@@ -593,15 +600,26 @@ class ExternalInputIterator(object):
             raise StopIteration
 
         for _ in range(self.batch_size):
-            jpeg_filename = self.files[self.i % self.n]
-            label = self.labels[self.i % self.n]
-            file = pil_loader(jpeg_filename)
-            batch.append(file)  # we can use numpy
-            labels.append(torch.tensor([int(label)], dtype = torch.int64)) # or PyTorch's native tensors
+            index = self.indexes[self.i % self.n]
+            jpeg_filename = self.files[index]
+            label = self.labels[index]
+            f = open(jpeg_filename, "rb")
+            file = np.frombuffer(f.read(), dtype=np.uint8)
+            batch.append(file)
+#             file = cp.asarray(imageio.imread(jpeg_filename))
+#             file = np.fromfile(jpeg_filename, dtype = np.uint8)
+#             batch.append(file.astype(cp.uint8))  # we can use numpy
+#             labels.append(torch.tensor([int(label)], dtype = torch.int64)) # or PyTorch's native tensors
+            labels.append(cp.array([label], dtype=np.uint8))
             if self.synthetic_data_path:
                 rand_int = random.randint(self.synthetic_index_min, self.synthetic_index_max)
-                synth_jpeg_filename = get_synthetic_image_path(jpeg_filename.absolute().as_posix(), self.synthetic_data_path, rand_int ,split="train")
-                synthetic_batch.append(pil_loader(synth_jpeg_filename))
+                synth_jpeg_filename = self.synth_files[rand_int][index]
+#                 synth_jpeg_filename = get_synthetic_image_path(jpeg_filename.absolute().as_posix(), self.synthetic_data_path, rand_int ,split="train")
+                f = open(synth_jpeg_filename, "rb")
+                synth_file = np.frombuffer(f.read(), dtype=np.uint8)
+#                 synth_file = cp.asarray(imageio.imread(synth_jpeg_filename))
+#                 np.fromfile(synth_jpeg_filename, dtype=np.uint8)
+                synthetic_batch.append(synth_file)
             else:
                 synthetic_batch.append(file)
 
@@ -661,18 +679,23 @@ class PretrainPipelineBuilder:
 
         super().__init__()
         self.synthetic_data_path = synthetic_data_path
-        self.batch_size = batch_size
+        self.batch_size = batch_size * num_shards
+        self.shard_id = shard_id
         self.num_threads = num_threads
         self.device_id = device_id
         self.seed = seed + device_id
+        
+        print("self.device_id",self.device_id, flush=True)
+        print("self.shard_id",self.shard_id, flush=True)
+        print("self.num_threads",self.num_threads, flush=True)
 
         self.device = device
 
         self.eei = ExternalInputIterator(data_path = data_path,
-                                         synthetic_data_path = synthetic_data_path,
+                                         synthetic_data_path = self.synthetic_data_path,
                                          synthetic_index_min = synthetic_index_min,
                                          synthetic_index_max = synthetic_index_max,
-                                         batch_size = batch_size,
+                                         batch_size = self.batch_size,
                                          shard_id = shard_id,
                                          random_shuffle = random_shuffle,
                                          num_shards = num_shards,
@@ -942,8 +965,10 @@ class PretrainDALIDataModule(pl.LightningDataModule):
         conversion_map = (
             train_pipeline_builder.eei.conversion_map if self.encode_indexes_into_labels else None
         )
+        print("self.batch_size", self.batch_size, flush= True)
+        print("self.num_shards", self.num_shards, flush= True)
         self.train_loader = PretrainWrapper(
-            model_batch_size=self.batch_size,
+            model_batch_size=self.batch_size*self.num_shards,
             model_rank=self.device_id,
             model_device=self.device,
             dataset_size=train_pipeline_builder.eei.data_set_len,
